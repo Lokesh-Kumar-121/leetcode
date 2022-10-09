34ee03db5672475dfb07893ef939b37f969483c6
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-10-10 02:11:40 +0530

    Time: 168 ms (5.09%), Space: 10.1 MB (59.33%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -5,8 +5,20 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
         
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
+        for(int i=1;i<n1;i++)
+        {
+            if(nums1[i] != nums1[i-1])
+                 sort(nums1.begin(),nums1.end());
+        }
+        
+        for(int i=1;i<n2;i++)
+        {
+            if(nums2[i] != nums2[i-1])
+                 sort(nums2.begin(),nums2.end());
+        }
+        
+        //sort(nums1.begin(),nums1.end());
+        //sort(nums2.begin(),nums2.end());
         
         vector<int>arr;
         // if(n1==0 || n2==0)
