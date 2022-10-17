319f235c14e39b62af312515fab43093a1aad66a
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-10-17 13:56:38 +0530

    Time: 113 ms (15.72%), Space: 27.6 MB (12.37%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        
+        int res=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        
+        if(res>=n || nums[res] != target )
+        return -1;
+        
+        else
+           return res;
+    }
+};
