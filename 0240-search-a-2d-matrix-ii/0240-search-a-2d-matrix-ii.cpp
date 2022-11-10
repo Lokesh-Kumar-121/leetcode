7c169061a0bfaa3b05942b185b0d5cf18cfa7e65
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-11-11 01:50:03 +0530

    Time: 277 ms (32.23%), Space: 15 MB (21.97%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -2,34 +2,51 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        vector<int>index;
+//         vector<int>index;
         
-        for(int i=0;i<matrix.size();i++)
-        {
+//         for(int i=0;i<matrix.size();i++)
+//         {
           
-            if(target >= matrix[i][0] && target <= matrix[i][matrix[i].size()-1])
-                index.push_back(i);
-        }
+//             if(target >= matrix[i][0] && target <= matrix[i][matrix[i].size()-1])
+//                 index.push_back(i);
+//         }
         
-        if(index.size() == 0)
-            return false;
+//         if(index.size() == 0)
+//             return false;
         
-        for(int i=0;i<index.size();i++)
-        {
-            int start=0,end=matrix[i].size()-1;
-            int mid;
+//         for(int i=0;i<index.size();i++)
+//         {
+//             int start=0,end=matrix[i].size()-1;
+//             int mid;
             
-            while(start <= end)
-            {
-                mid=(start+end)/2;
+//             while(start <= end)
+//             {
+//                 mid=(start+end)/2;
                 
-                if(matrix[index[i]][mid] == target)    return true;
+//                 if(matrix[index[i]][mid] == target)    return true;
                 
-                else if(matrix[index[i]][mid] < target)    start=mid+1;
+//                 else if(matrix[index[i]][mid] < target)    start=mid+1;
                 
-                else    end=mid-1;
+//                 else    end=mid-1;
                     
-            }
+//             }
+//         }
+        
+//         return false;
+        
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        int r=0,c=col-1;
+        
+        while(r < row && c >= 0)
+        {
+            if(matrix[r][c] == target)  return true;
+            
+            else if(matrix[r][c] > target)  c--;
+            
+            else    r++;
         }
         
         return false;
