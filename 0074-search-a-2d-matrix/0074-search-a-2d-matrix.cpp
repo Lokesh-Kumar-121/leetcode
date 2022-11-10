d01150414087978d6c8360d129fd569015effaac
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-11-11 01:48:59 +0530

    Time: 3 ms (95.06%), Space: 9.6 MB (5.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,34 +2,53 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
      
-        int findrow=-1;
-        for(int i=0;i<matrix.size();i++)
-        {
+//         int findrow=-1;
+//         for(int i=0;i<matrix.size();i++)
+//         {
            
-               if(target >= matrix[i][0] && target <= matrix[i][matrix[i].size()-1])
-               {
-                   findrow=i;
-                   break;
-               }
+//                if(target >= matrix[i][0] && target <= matrix[i][matrix[i].size()-1])
+//                {
+//                    findrow=i;
+//                    break;
+//                }
             
-        }
+//         }
         
-        //cout<<findrow<<endl;
-        if(findrow==-1)
-            return false;
         
-        int i=0,j=matrix[findrow].size()-1;
-        int mid;
+       
         
-        while(i<=j)
+//         //cout<<findrow<<endl;
+//         if(findrow==-1)
+//             return false;
+        
+//         int i=0,j=matrix[findrow].size()-1;
+//         int mid;
+        
+//         while(i<=j)
+//         {
+//             mid=(i+j)/2;
+//             if(matrix[findrow][mid] == target)
+//                 return true;
+//             else if(matrix[findrow][mid] < target)
+//                 i=mid+1;
+//             else
+//                 j=mid-1;
+//         }
+        
+//         return false;
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        
+        int r=0,c=col-1;
+        
+        while(r < row && c >= 0)
         {
-            mid=(i+j)/2;
-            if(matrix[findrow][mid] == target)
-                return true;
-            else if(matrix[findrow][mid] < target)
-                i=mid+1;
-            else
-                j=mid-1;
+            if(matrix[r][c] == target)  return true;
+            
+            else if(matrix[r][c] > target)  c--;
+            
+            else    r++;
         }
         
         return false;
