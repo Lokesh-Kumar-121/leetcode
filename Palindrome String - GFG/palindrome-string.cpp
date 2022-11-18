39180b05b47e0bc9b7184476bc3bd4318952c823
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-11-18 10:14:54 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -9,12 +9,25 @@ using namespace std;
 class Solution{
 public:	
 	
+string _reverse(string str)
+	{
+	    int n=str.length();
+	    int i=0,j=n-1;
+	    
+	    while(i<j)
+	    {
+	        swap(str[i],str[j]);
+	        i++;j--;
+	    }
+	    
+	    return str;
+	}
 	
 	int isPalindrome(string S)
 	{
 	    // Your code goes here
-	    string Rs=S;
-	    reverse(Rs.begin(),Rs.end());
+	    string Rs= _reverse(S);
+	   
 	    
 	    if(S==Rs)
 	        return 1;
