5078d28427ffab9b57151473d4dec3fdaf002c87
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-11-20 15:53:00 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -8,28 +8,71 @@ class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
         // code here
-        int diff1=INT_MIN;
-        //int diff2;
+    //     int diff1=INT_MIN;
+    //     //int diff2;
         
-        int i=0,j=n-1;
+    //     int i=0,j=n-1;
        
-       while(i<n)
-       {
-           while(1)
-           {
-               if(arr[j] >= arr[i])
-               {
-                   //cout<<arr[j]<<" "<<arr[i]<<endl;
-                   diff1=max(diff1,j-i);
-                   break;
-               }
-               j--;
-           }
-           j=n-1;
-           i++;
-       }
+    //   while(i<n)
+    //   {
+    //       while(1)
+    //       {
+    //           if(arr[j] >= arr[i])
+    //           {
+    //               //cout<<arr[j]<<" "<<arr[i]<<endl;
+    //               diff1=max(diff1,j-i);
+    //               break;
+    //           }
+    //           j--;
+    //       }
+    //       j=n-1;
+    //       i++;
+    //   }
         
-        return diff1;
+    //     return diff1;
+    
+    
+         int lmin[n];
+         int rmax[n];
+         
+         lmin[0]=arr[0];
+         for(int i=1;i<n;i++)
+         {
+             lmin[i]=min(arr[i],lmin[i-1]);
+         }
+         
+         rmax[n-1]=arr[n-1];
+         for(int i=n-2;i>=0;i--)
+         {
+             rmax[i]=max(arr[i],rmax[i+1]);
+         }
+         
+        //  for(auto x:lmin)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        //   for(auto x:rmax)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        
+        
+        int i=0,j=0;
+        int diff=INT_MIN;
+        while(i<n && j<n)
+        {
+            if(rmax[j] >= lmin[i])
+            {
+                diff=max(diff,j-i);
+                j++;
+            }
+            else
+                i++;
+        }
+        
+        
+        return diff;
+         
+         
+         
     }
 
 };
