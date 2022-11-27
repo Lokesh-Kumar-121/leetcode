973a69ad6928fb842f3349866f219c23dfd87ffb
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-11-28 00:35:48 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,37 +2,64 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        int c0=0,c1=0,c2=0;
-        for(int i=0;i<nums.size();i++)
+//         int c0=0,c1=0,c2=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i] == 0)
+//                 c0++;
+//             else if(nums[i] == 1)
+//                 c1++;
+//             else
+//                 c2++;
+//         }
+        
+//        // cout<<c0<<c1<<c2;
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(c0 != 0){
+//                 nums[i]=0;
+//                 c0--;
+//             }
+//             else if(c1 != 0)
+//             {
+//                 nums[i] = 1;
+//                 c1--;
+//             }
+//             else
+//             {
+//                 nums[i]=2;
+//                 c2--;
+//             }
+//         }
+        
+        int ind=0;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
         {
             if(nums[i] == 0)
-                c0++;
-            else if(nums[i] == 1)
-                c1++;
-            else
-                c2++;
+            {
+                swap(nums[i],nums[ind]);
+                ind++;
+            }
         }
-        
-       // cout<<c0<<c1<<c2;
-        
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            if(c0 != 0){
-                nums[i]=0;
-                c0--;
-            }
-            else if(c1 != 0)
+            if(nums[i] == 1)
             {
-                nums[i] = 1;
-                c1--;
-            }
-            else
-            {
-                nums[i]=2;
-                c2--;
+                swap(nums[i],nums[ind]);
+                ind++;
             }
         }
-        
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i] == 0)
+        //     {
+        //         swap(nums[i],nums[ind]);
+        //         ind++;
+        //     }
+        // }
         
     }
 };
