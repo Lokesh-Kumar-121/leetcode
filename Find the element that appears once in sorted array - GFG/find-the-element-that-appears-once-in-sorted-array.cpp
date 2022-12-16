5aa72cd6cd4eabcd7cfdc233a69c6d76899719ec
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2022-12-16 21:15:25 +0530

    Added solution - LeetHub

--- Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
+++ Find the element that appears once in sorted array - GFG/find-the-element-that-appears-once-in-sorted-array.cpp
@@ -12,30 +12,30 @@ class Solution
     int findOnce(int arr[], int n)
     {
         //code here.
-        // int left=0,right=0;
-        // int i=0,j=n-1;
-        // while(i<j)
-        // {
-        //     //cout<<left<<" ";
-        //     left=left^arr[i];
-        //     i++;
-        //     //cout<<right<<endl;
-        //     right=right^arr[j];
-        //     j--;
-        // }
-        // //cout<<left<<" "<<right<<endl;
-        // if(n&1)
-        //     left^=arr[n/2];
-        
-        // return (left^right);
-        
-        
-        int res=0;
-        for(int i=0;i<n;i++)
+        int left=0,right=0;
+        int i=0,j=n-1;
+        while(i<j)
         {
-            res^=arr[i];
+            //cout<<left<<" ";
+            left=left^arr[i];
+            i++;
+            //cout<<right<<endl;
+            right=right^arr[j];
+            j--;
         }
-        return res;
+        //cout<<left<<" "<<right<<endl;
+        if(n&1)
+            left^=arr[n/2];
+        
+        return (left^right);
+        
+        
+        // int res=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     res^=arr[i];
+        // }
+        // return res;
     }
 };
 
