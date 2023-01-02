f3ac221ef241efa4907fbf75472d24e16d5371a6
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-01-02 17:23:09 +0530

    Time: 11 ms (40.74%), Space: 8.4 MB (43.71%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -5,7 +5,15 @@ public:
         vector<int>res;
         for(int i=0;i<=n;i++)
         {
-            int ans=__builtin_popcount(i);
+            // int ans=__builtin_popcount(i);
+            int ans=0;
+            int temp=i;
+            while(temp!=0)
+            {
+                if(temp&1)
+                    ans+=1;
+                temp=temp>>1;
+            }
             res.push_back(ans);
         }
         return res;
