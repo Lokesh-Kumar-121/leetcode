918169e20fe72924ed6862354a690bbea342017a
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-01-11 01:30:21 +0530

    Time: 51 ms (97.17%), Space: 33.8 MB (64.59%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        unordered_set<int>sett;
         vector<int>res;
+//         unordered_set<int>sett;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             sett.insert(nums[i]);
+//         }
+        
+//         for(int i=1;i<=nums.size();i++)
+//         {
+//             if(sett.find(i) == sett.end())
+//                 res.push_back(i);
+//         }
+//         return res;
+        
         for(int i=0;i<nums.size();i++)
         {
-            sett.insert(nums[i]);
+            nums[abs(nums[i])-1]=-abs(nums[abs(nums[i])-1]);
         }
+        // for(auto x:nums)
+        //     cout<<x<<" ";
         
-        for(int i=1;i<=nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(sett.find(i) == sett.end())
-                res.push_back(i);
+            if(nums[i] > 0)
+                res.push_back(i+1);
         }
         return res;
+        
     }
 };
