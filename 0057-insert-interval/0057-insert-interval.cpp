d5ca6f06aaba46b70b67de699082dca593e6acd1
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-01-17 00:16:53 +0530

    Time: 11 ms (97.45%), Space: 17.1 MB (78.65%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,62 @@
+// class Solution {
+// public:
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+//         vector<vector<int>>res;
+//         int i=0;
+//         while(intervals[i][1] < newInterval[0])
+//         {
+//             res.push_back(intervals[i]);
+//             i++;
+//         }
+        
+//         pair<int,int>merge={0,0};
+//         merge.first=min(intervals[i][0],newInterval[0]);
+        
+//         while(i<intervals.size()-1 && newInterval[1] >= intervals[i][1] && newInterval[1] <= intervals[i+1][0])
+//         {
+//             i++;
+//         }
+//         i--;
+//         merge.second=max(intervals[i][1],newInterval[1]);
+//         res.push_back({merge.first,merge.second});
+//         i++;
+//         while(i<intervals.size())
+//         {
+//             res.push_back(intervals[i]);
+//             i++;
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        for(int i=intervals.size()-2;i>=0;i--){
+            if(intervals[i][0]>newInterval[0]){
+                swap(intervals[i],intervals[i+1]);
+            }
+            else if(intervals[i][0]==newInterval[0]){
+                if(intervals[i][1]>newInterval[1]){
+                    swap(intervals[i],intervals[i+1]);
+                }
+            }
+            else{
+                break;
+            }
+        }
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(ans[ans.size()-1][1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans[ans.size()-1][1]=max(ans[ans.size()-1][1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
