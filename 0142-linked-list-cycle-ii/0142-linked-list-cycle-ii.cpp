4f84eceb61296e6165c7930530ac04e7cf11d2ae
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-01-20 15:14:33 +0530

    Time: 7 ms (90.19%), Space: 7.5 MB (82.38%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,28 +9,35 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-     
-         if (head == NULL || head->next == NULL) return NULL;
-    
-    ListNode* firstp = head;
-    ListNode* secondp = head;
-    bool isCycle = false;
-    
-    while(firstp != NULL && secondp != NULL) {
-        firstp = firstp->next;
-        if (secondp->next == NULL) return NULL;
-        secondp = secondp->next->next;
-        if (firstp == secondp) { isCycle = true; break; }
-    }
-    
-    if(!isCycle) return NULL;
-    firstp = head;
-    while( firstp != secondp) {
-        firstp = firstp->next;
-        secondp = secondp->next;
-    }
-
-    return firstp;
+       // ListNode* temp=new ListNode(-1);
+        if(head==NULL || head->next==NULL)
+        {
+            return NULL;
+        }
         
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                break;
+        }
+        if(fast==NULL || fast->next==NULL)
+        {
+            return NULL;
+        }
+        
+        while(slow != head)
+        {
+            head=head->next;
+            slow=slow->next;
+            
+        }
+        
+        return head;
     }
 };
