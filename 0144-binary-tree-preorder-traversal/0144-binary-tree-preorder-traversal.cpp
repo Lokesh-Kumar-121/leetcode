2762d41c0da1fa9dd3f981bca3f58e176c05a23f
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-01-25 01:29:37 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (50.15%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -12,19 +12,38 @@
 class Solution {
 public:
     
-    void func(TreeNode* root,vector<int>&res)
-    {
-        if(!root)
-            return;
-        //cout<<root->val<<" ";
-        res.push_back(root->val);
-        func(root->left,res);
-        func(root->right,res);
-    }
+    // void func(TreeNode* root,vector<int>&res)
+    // {
+    //     if(!root)
+    //         return;
+    //     //cout<<root->val<<" ";
+    //     res.push_back(root->val);
+    //     func(root->left,res);
+    //     func(root->right,res);
+    // }
     
     vector<int> preorderTraversal(TreeNode* root) {
+        // vector<int>res;
+        // func(root,res);
+        // return res;
+        
+        //*****using stack*****//itreative one
+        if(!root)
+            return {};
         vector<int>res;
-        func(root,res);
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty())
+        {
+            root=st.top();
+            st.pop();
+            res.push_back(root->val);
+            
+            if(root->right)
+                st.push(root->right);
+            if(root->left)
+                st.push(root->left);
+        }
         return res;
     }
 };
