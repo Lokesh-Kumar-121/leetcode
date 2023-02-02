44bad55e4ae7a225b017b619da57d2840ec1bf79
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-02 22:52:50 +0530

    Time: 8 ms (43.80%), Space: 9.7 MB (25.79%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-          int mapping[26];
-        for (int i = 0; i < 26; i++)
-            mapping[order[i] - 'a'] = i;
-        for (string &w : words)
-            for (char &c : w)
-                c = mapping[c - 'a'];
-        return is_sorted(words.begin(), words.end());
+        
+        unordered_map<char,int>mapp;
+        for(int i=0;i<order.length();i++)
+        {
+            mapp[order[i]] = i;
+        }
+        
+        // for(auto x:mapp)
+        //     cout<<x.first<<" "<<x.second<<endl;
+        
+        for(int i=1;i<words.size();i++)
+        {
+            string s1=words[i-1];
+            string s2=words[i];
+            cout<<s1<<" "<<s2<<endl;
+            int n1=s1.length();
+            int n2=s2.length();
+            
+            int j=0;
+            while(j<n1 && j<n2)
+            {
+                //cout<<s1[j]<<" "<<s2[j]<<"  ";
+                if(mapp[s1[j]] > mapp[s2[j]])
+                    return false;
+                if(mapp[s1[j]] < mapp[s2[j]])
+                    break;
+                if(mapp[s1[j]] == mapp[s2[j]])
+                    j++;
+            }
+            
+            if(j==n2 && j!= n1)
+                return false;
+            
+        }
+        
+        return true;
     }
 };
