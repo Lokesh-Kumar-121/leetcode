795e0f781ff5d4281b8227c48e44f3d44b2a9a91
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-07 02:27:14 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Code here
+        int flag1=0,flag2=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j] == 1)
+                    flag1=1;
+                if(grid[i][j] == 2)
+                    flag2=1;
+            }
+        }
+        
+        if(flag1 == 0 && flag2 ==0)
+            return 0;
+        if(flag1 == 1 && flag2 == 0)
+            return -1;
+        if(flag1 == 0 && flag2 == 1)
+            return 0;
+        
+        queue<pair<int,int>>q;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j] == 2)
+                    q.push({i,j});
+            }
+        }
+        
+        int count=0;
+        while(!q.empty())
+        {
+            count++;
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                
+            pair<int,int>pp=q.front();
+            q.pop();
+            
+            int row=pp.first;
+            int col=pp.second;
+            
+            if(row < grid.size()-1 && grid[row+1][col] == 1){
+                grid[row+1][col] = 2;
+                q.push({row+1,col});
+            }
+            if(col < grid[0].size()-1 && grid[row][col+1] == 1){
+                grid[row][col+1] = 2;
+                q.push({row,col+1});
+            }
+            if(row > 0 && grid[row-1][col] == 1){
+                grid[row-1][col] = 2;
+                q.push({row-1,col});
+            }
+            if(col > 0 && grid[row][col-1] == 1){
+                grid[row][col-1] = 2;
+                q.push({row,col-1});
+            }
+                
+            }
+        }
+        
+        
+        for(auto x:grid)
+            for(auto z:x)
+                if(z == 1)
+                    return -1;
+        
+        return count-1;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
