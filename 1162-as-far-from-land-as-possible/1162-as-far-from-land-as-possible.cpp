a2088787abe07555b65d720ed3921062b498bc96
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-11 03:56:57 +0530

    Time: 210 ms (15.45%), Space: 50.5 MB (5.32%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,182 +1,60 @@
-// class Solution {
-// public:
-    
-    
-// //     int bfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&vis)
-// //     {
-// //         int count=0;
-// //         // vis[row][col] = 1;
-// //         queue<pair<int,int>>q;
-// //         q.push({row,col});
-        
-// //         int flag=0;
-// //         while(!q.empty())
-// //         {
-// //             count++;
-// //             int n=q.size();
-// //             for(int i=0;i<n;i++)
-// //             {
-// //                 int x=q.front().first;
-// //                 int y=q.front().second;
-// //                 q.pop();
-// //                 if(grid[x][y] == 1)
-// //                 {
-// //                     flag=1;
-// //                     break;
-                    
-// //                 }
-                
-// //                 if(x < grid.size()-1 && vis[x+1][y] == 0)
-// //                 {
-// //                     q.push({x+1,y});
-// //                     // vis[x+1][y] = 1;
-// //                 }  
-// //                 if(y < grid[0].size()-1 && vis[x][y+1] == 0)
-// //                 {
-// //                     q.push({x,y+1});
-// //                     // vis[x][y+1] = 1;
-// //                 }
-                    
-// //                 if(x > 0 && vis[x-1][y] == 0)
-// //                 {
-// //                     q.push({x-1,y});
-// //                     // vis[x-1][y] = 1;
-// //                 }    
-                    
-// //                 if(y > 0 && vis[x][y-1] == 0)
-// //                 {
-// //                     q.push({x,y-1});
-// //                     // vis[x][y-1] = 1;
-// //                 }    
-                    
-// //             }
-            
-// //             if(flag==1)
-// //                 break;
-// //         }
-        
-// //         return count-1;
-// //     }
-    
-    
-//     int bfs(int row,int col,vector<vector<int>>&grid)
-//     {
-//         int count=0;
-//         queue<pair<int,int>>q;
-//         q.push({row,col});
-        
-//         while(!q.empty())
-//         {
-//             count++;
-//             int n=q.size();
-//             for(int i=0;i<n;i++)
-//             {
-//                 int x=q.front().first;
-//                 int y=q.front().second;
-//                 q.pop();
-                
-//                 if(x < grid.size()-1 && grid[x+1][y] == 0)
-//                     q.push({x+1,y});
-//                 if(y < grid.size()-1 && grid[x][y+1] == 0)
-//                     q.push({x,y+1});
-//                 // if(x > 0 && grid[x-1][y] == 0)
-//                 //     q.push({x-1,y});
-//                 // if(y > 0 && grid[x][y-1] == 0)
-//                 //     q.push({x,y-1});
-//             }
-//         }
-        
-//         return count-1;
-//     }
-    
-//     int maxDistance(vector<vector<int>>& grid) {
-        
-// //         vector<vector<int>>vis(grid.size(),vector<int>(grid.size(),0));
-// //         // for(auto x:vis)
-// //         // {   for(auto z:x)
-// //         //         cout<<z<<" ";
-// //         //     cout<<endl;
-// //         // }
-// //         // cout<<endl;
-// //         int ans=-1;
-// //         for(int i=0;i<grid.size();i++)
-// //         {
-// //             for(int j=0;j<grid[0].size();j++)
-// //             {
-// //                 if(grid[i][j] == 0)
-// //                 {
-// //                     ans=max(ans,bfs(i,j,grid,vis));
-// //                 }
-// //             }
-// //         }
-        
-// //         return ans;
-    
-        
-//         int ans=-1;
-//         for(int i=0;i<grid.size();i++)
-//         {
-//             for(int j=0;j<grid[0].size();j++)
-//             {
-//                 if(grid[i][j] == 1)
-//                 {
-//                     ans=max(ans,bfs(i,j,grid));
-//                 }
-//             }
-//         }
-        
-//         return ans;
-//     }
-        
-// };
-
-
-class point{
-    public:
-    int x;
-    int y;
-    point(int X,int Y){
-        x=X;y=Y;
-    }
-};
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        queue<point> q;
-        int n = grid.size();
-        vector<vector<int>> map(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]){
-                    q.push(point(i,j));
-                    map[i][j]=0;
+        
+        int n=grid.size();
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        int res=-1;
+        
+        queue<vector<int>>q;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j] == 1)
+                {
+                    q.push({i,j,0});
+                    vis[i][j] = 1;
                 }
             }
         }
-        int maxi = -1;
-        while(!q.empty()){
-            point temp = q.front();
+        
+        if(q.size() == n*n)
+            return -1;
+        
+        while(!q.empty())
+        {
+            vector<int>temp=q.front();
             q.pop();
-            int x = temp.x;
-            int y = temp.y;
-            maxi = max(maxi,map[x][y]);
-            if(x>0 && map[x-1][y]<0){
-                map[x-1][y] = map[x][y]+1;
-                q.push(point(x-1,y));
+            int row=temp[0];
+            int col=temp[1];
+            int dist=temp[2];
+            
+            res=max(res,dist);
+            
+            if(row < grid.size()-1 && vis[row+1][col] == 0)
+            {
+                q.push({row+1,col,dist+1});
+                vis[row+1][col] = 1;
             }
-            if(x<n-1 && map[x+1][y]<0){
-                map[x+1][y] = map[x][y]+1;
-                q.push(point(x+1,y));
+            if(col < grid[0].size()-1 && vis[row][col+1] == 0)
+            {
+                q.push({row,col+1,dist+1});
+                vis[row][col+1] = 1;
             }
-            if(y>0 && map[x][y-1]<0){
-                map[x][y-1] = map[x][y]+1;
-                q.push(point(x,y-1));
+            if(row > 0 && vis[row-1][col] == 0)
+            {
+                q.push({row-1,col,dist+1});
+                vis[row-1][col] = 1; 
             }
-            if(y<n-1 && map[x][y+1]<0){
-                map[x][y+1] = map[x][y]+1;
-                q.push(point(x,y+1));
+            if(col > 0 && vis[row][col-1] == 0)
+            {
+                q.push({row,col-1,dist+1});
+                vis[row][col-1] = 1; 
             }
         }
-        return maxi==0?-1:maxi;
+        
+        return res;
     }
 };
