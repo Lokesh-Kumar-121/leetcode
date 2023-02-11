0e759e931b26d8e9d6d64dca4b3bb1fbc1b3575e
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-12 05:13:42 +0530

    Time: 134 ms (60.46%), Space: 61.3 MB (68.36%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -16,20 +16,41 @@ public:
         
 //         return x;
         
+        ///////hashmap/////
+//         int n=nums.size();
+//         vector<int>hash(n,0);
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             hash[nums[i]]++;
+//         }
         
-        int n=nums.size();
-        vector<int>hash(n,0);
-        for(int i=0;i<nums.size();i++)
+//         for(int i=0;i<n;i++)
+//         {
+//             if(hash[i] > 1)
+//                 return i;
+//         }
+//         return -1;
+        
+        ///////CYCLIC SORT//////
+        
+        int i=0;
+        while(i<nums.size())
         {
-            hash[nums[i]]++;
+            if(nums[i] != i+1)
+            {
+                if(nums[i] != nums[nums[i] - 1])
+                    swap(nums[i],nums[nums[i] - 1]);
+                else
+                    return nums[i];
+            }
+            else
+                i++;
+            
         }
         
-        for(int i=0;i<n;i++)
-        {
-            if(hash[i] > 1)
-                return i;
-        }
         return -1;
+        
+        
     }
 };
 
