20cca7c065b990df9c558a94a0234adf0f403207
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-12 04:13:57 +0530

    Time: 67 ms (59.25%), Space: 33.8 MB (50.72%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -15,19 +15,41 @@ public:
 //         }
 //         return res;
         
-        for(int i=0;i<nums.size();i++)
-        {
-            nums[abs(nums[i])-1]=-abs(nums[abs(nums[i])-1]);
-        }
-        // for(auto x:nums)
-        //     cout<<x<<" ";
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             nums[abs(nums[i])-1]=-abs(nums[abs(nums[i])-1]);
+//         }
+//         // for(auto x:nums)
+//         //     cout<<x<<" ";
         
-        for(int i=0;i<nums.size();i++)
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i] > 0)
+//                 res.push_back(i+1);
+//         }
+//         return res;
+        
+        
+        //////////CYCLIC SORT//////////
+        
+        int n=nums.size();
+        int i=0;
+        while(i<n)
         {
-            if(nums[i] > 0)
+            if(nums[i] != i+1 && nums[i] != nums[nums[i] - 1])
+            {
+                swap(nums[i],nums[nums[i] - 1]);
+            }
+            else
+                i++;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] != i+1)
                 res.push_back(i+1);
         }
-        return res;
         
+        return res;
     }
 };
