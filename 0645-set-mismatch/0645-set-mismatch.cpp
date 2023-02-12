035455c572dac05c83f20d42fd70694f76c023d6
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-12 05:44:52 +0530

    Time: 23 ms (97.16%), Space: 22.2 MB (53.67%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,25 +2,49 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         
-        int i=0;
-        while(i<nums.size())
-        {
-            if(nums[i] != i+1 && nums[i] != nums[nums[i] - 1])
-                swap(nums[i],nums[nums[i] - 1]);
-            else
-                i++;
-        }
-        
+        vector<int>res;
+        vector<int>hash(nums.size(),0);
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i] != i+1)
-                return {nums[i] , i+1};
-                
+            hash[nums[i] - 1]++;
         }
         
-        return {};
+        for(int i=0;i<hash.size();i++)
+        {
+            if(hash[i] > 1)
+                res.push_back(i+1);
+        }
+        for(int i=0;i<hash.size();i++)
+        {
+            if(hash[i] == 0)
+                res.push_back(i+1);
+        }
+        
+        return res;
+        
+        
+        // Cyclic sort////////
+//         int i=0;
+//         while(i<nums.size())
+//         {
+//             if(nums[i] != i+1 && nums[i] != nums[nums[i] - 1])
+//                 swap(nums[i],nums[nums[i] - 1]);
+//             else
+//                 i++;
+//         }
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i] != i+1)
+//                 return {nums[i] , i+1};
+                
+//         }
+        
+//         return {};
     }
 };
 
 // 0 1 2 3
 // 1 2 2 4
+
+
