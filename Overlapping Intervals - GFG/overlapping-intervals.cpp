3a51165530c39d9ebf3acb334fb09eb3bc15f6a7
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-14 23:41:35 +0530

    Added solution - LeetHub

--- Overlapping Intervals - GFG/overlapping-intervals.cpp
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -9,13 +9,23 @@ public:
          // Code here
          sort(arr.begin(),arr.end());
          vector<vector<int>>res;
+         vector<int>ans;
+         int count=1;
+         ans.push_back(count);
          for(int i=1;i<arr.size();i++)
          {
+            int flag=0;
             if(arr[i][0] >= arr[i-1][0] && arr[i][0] <= arr[i-1][1])
             {
+                flag=1;
                 arr[i][0]=min(arr[i][0],arr[i-1][0]);
                 arr[i][1]=max(arr[i][1],arr[i-1][1]);
             }
+            if(flag==0)
+            {
+                count++;
+            }
+            ans.push_back(count);
          }
          
         // for(auto x:arr)
@@ -33,6 +43,12 @@ public:
             }
         }
         res.push_back(arr[arr.size()-1]);
+        
+        // for(auto x:ans)
+        //     cout<<x<<" ";
+        // cout<<endl;
+        
+        
         
         return res;
     }
