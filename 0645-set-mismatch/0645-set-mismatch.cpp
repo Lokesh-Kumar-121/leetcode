6d41dabb3a690b6b4b73ca1499246b29c6624927
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-15 01:15:06 +0530

    Time: 116 ms (11.95%), Space: 21.4 MB (89.61%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        
-        vector<int>res;
-        vector<int>hash(nums.size(),0);
-        for(int i=0;i<nums.size();i++)
+        int n = nums.size();
+        int rep = 0;
+        for(int i = 0;i<n;i++)
         {
-            hash[nums[i] - 1]++;
+            if(nums[abs(nums[i])-1]<0)
+            {
+                rep = abs(nums[i]);
+            }
+            else 
+            {
+                nums[abs(nums[i])-1] = -1* nums[abs(nums[i])-1];
+            }
+            
         }
-        
-        for(int i=0;i<hash.size();i++)
+        for(int i = 0;i<n;i++)
         {
-            if(hash[i] > 1)
-                res.push_back(i+1);
+            cout<<nums[i]<<" ";
         }
-        for(int i=0;i<hash.size();i++)
+        int mis = 0;
+        for(int i=0;i<n;i++)
         {
-            if(hash[i] == 0)
-                res.push_back(i+1);
+            if(nums[i] > 0)
+            {
+                mis = i+1;
+                break;
+            }
         }
+        vector<int> ans;
+        ans.push_back(rep);
+        ans.push_back(mis);
+        return ans;
         
-        return res;
         
-        
-        // Cyclic sort////////
-//         int i=0;
-//         while(i<nums.size())
-//         {
-//             if(nums[i] != i+1 && nums[i] != nums[nums[i] - 1])
-//                 swap(nums[i],nums[nums[i] - 1]);
-//             else
-//                 i++;
-//         }
-        
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             if(nums[i] != i+1)
-//                 return {nums[i] , i+1};
-                
-//         }
-        
-//         return {};
     }
 };
-
-// 0 1 2 3
-// 1 2 2 4
-
-
