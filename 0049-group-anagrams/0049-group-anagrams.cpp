350d5be72cf67694274d8830ad7e84dbca58396e
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-17 10:30:03 +0530

    Time: 2905 ms (5.02%), Space: 21.1 MB (28.39%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n=strs.size();
+        vector<vector<int>>freq(n,vector<int>(26,0));
+        
+        for(int i=0;i<n;i++)
+        {
+            string s=strs[i];
+            for(int j=0;j<s.length();j++)
+            {
+                freq[i][s[j] - 'a']++;
+            }
+        }
+        
+        vector<vector<string>>res;
+        vector<int>vis(n,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i] == 0)
+            {
+                vis[i] = 1;
+            vector<string>temp;
+            temp.push_back(strs[i]);
+            for(int j=i+1;j<n;j++)
+            {
+                if(freq[i] == freq[j])
+                {
+                    temp.push_back(strs[j]);
+                    vis[j] = 1;
+                }
+            }
+            res.push_back(temp);
+            }
+        }
+        
+        return res;
+    }
+};
