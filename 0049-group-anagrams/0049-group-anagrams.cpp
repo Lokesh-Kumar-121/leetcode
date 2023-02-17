b08d254fae408f61cbbac2f002a9e89cfd426cad
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-02-17 10:39:46 +0530

    Time: 37 ms (75.90%), Space: 20.9 MB (35.19%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n=strs.size();
-        vector<vector<int>>freq(n,vector<int>(26,0));
+//         int n=strs.size();
+//         vector<vector<int>>freq(n,vector<int>(26,0));
         
+//         for(int i=0;i<n;i++)
+//         {
+//             string s=strs[i];
+//             for(int j=0;j<s.length();j++)
+//             {
+//                 freq[i][s[j] - 'a']++;
+//             }
+//         }
+        
+//         vector<vector<string>>res;
+//         vector<int>vis(n,0);
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(vis[i] == 0)
+//             {
+//                 vis[i] = 1;
+//             vector<string>temp;
+//             temp.push_back(strs[i]);
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(freq[i] == freq[j])
+//                 {
+//                     temp.push_back(strs[j]);
+//                     vis[j] = 1;
+//                 }
+//             }
+//             res.push_back(temp);
+//             }
+//         }
+        
+//         return res;
+        int n=strs.size();
+        map<string,vector<string>>mapp;
         for(int i=0;i<n;i++)
         {
-            string s=strs[i];
-            for(int j=0;j<s.length();j++)
-            {
-                freq[i][s[j] - 'a']++;
-            }
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            mapp[s].push_back(strs[i]);
         }
         
         vector<vector<string>>res;
-        vector<int>vis(n,0);
-        
-        for(int i=0;i<n;i++)
+        for(auto x : mapp)
         {
-            if(vis[i] == 0)
-            {
-                vis[i] = 1;
-            vector<string>temp;
-            temp.push_back(strs[i]);
-            for(int j=i+1;j<n;j++)
-            {
-                if(freq[i] == freq[j])
-                {
-                    temp.push_back(strs[j]);
-                    vis[j] = 1;
-                }
-            }
-            res.push_back(temp);
-            }
+            res.push_back(x.second);
         }
         
         return res;
