0267bd8b63af79c8d943c82cbca8bcb0f88c8a50
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-03-01 01:25:45 +0530

    Added solution - LeetHub

---
+++ Word Ladder I - GFG/word-ladder-i.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
+        // Code here
+        unordered_set<string>sett;
+        for(int i=0;i<wordList.size();i++)
+        {
+            if(wordList[i] != startWord)
+                sett.insert(wordList[i]);
+        }
+        if(sett.find(targetWord) == sett.end())
+            return 0;
+        
+        queue<pair<string,int>>q;
+        q.push({startWord,1});
+        
+        while(!q.empty())
+        {
+            string temp=q.front().first;
+            // cout<<temp<<endl;
+            int count=q.front().second;
+            q.pop();
+            for(int i=0;i<temp.length();i++)
+            {
+                char xx = temp[i];
+                for(int j=0;j<26;j++)
+                {
+                    if(temp[i] == j+'a')
+                        continue;
+                    temp[i] = j + 'a';
+                    
+                    if(temp == targetWord)
+                        return count+1;
+                    if(sett.find(temp) != sett.end())
+                    {
+                        q.push({temp,count+1});
+                        sett.erase(temp);
+                    }
+                }
+                temp[i] = xx;
+                
+            }
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
