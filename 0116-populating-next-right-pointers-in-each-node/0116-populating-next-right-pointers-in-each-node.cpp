cfac671b3a3a8d46f277348a8b961345e78526b5
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-04-12 00:48:12 +0530

    Time: 21 ms (58.76%), Space: 17.5 MB (14.42%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -20,37 +20,82 @@ class Solution {
 public:
     Node* connect(Node* root) {
         
+//         if(!root)
+//             return root;
+        
+//         queue<Node*>q;
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+//             vector<Node*>arr;
+//             for(int i=0;i<n;i++)
+//             {
+//                 Node* temp=q.front();
+//                 arr.push_back(temp);
+//                 q.pop();
+//                 if(q.empty())
+//                     temp->next=NULL;
+//                 else
+//                     temp->next=q.front();
+                
+//             }
+//             for(auto x:arr)
+//             {
+//                 if(x->left)
+//                     q.push(x->left);
+//                 if(x->right)
+//                     q.push(x->right);
+//             }
+//         }
+        
+//         return root;
+        
         if(!root)
             return root;
-        
-        queue<Node*>q;
-        q.push(root);
+        root->next=NULL;
+        queue<pair<Node*,Node*>>q;
+        if(root->right)
+            q.push({root->right,root});
+        if(root->left)
+            q.push({root->left,root});
         
         while(!q.empty())
         {
-            int n=q.size();
-            vector<Node*>arr;
-            for(int i=0;i<n;i++)
-            {
-                Node* temp=q.front();
-                arr.push_back(temp);
+            // int n=q.size();
+            
+            // for(int i=0;i<n;i++)
+            // {
+                Node* parent = q.front().second;
+                Node* temp = q.front().first;
                 q.pop();
-                if(q.empty())
-                    temp->next=NULL;
-                else
-                    temp->next=q.front();
                 
-            }
-            for(auto x:arr)
-            {
-                if(x->left)
-                    q.push(x->left);
-                if(x->right)
-                    q.push(x->right);
-            }
+                if(parent->right && temp != parent->right)
+                    temp->next = parent->right;
+                
+                else if(parent->next == NULL)
+                    temp->next = NULL;
+                    
+                else if(parent->next->left)
+                    temp->next = parent->next->left;
+                    
+                else if(parent->next->right)
+                    temp->next = parent->next->right;
+                    
+                else
+                    temp->next = NULL;
+                
+                
+                if(temp->right)
+                    q.push({temp->right,temp});
+                if(temp->left)
+                    q.push({temp->left,temp});
+            // }
         }
         
         return root;
-        
     }
+
+    
 };
