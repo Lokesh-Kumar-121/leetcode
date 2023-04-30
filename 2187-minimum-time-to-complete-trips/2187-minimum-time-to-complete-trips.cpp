1efef8af596bda4cafc1a0f5ec805ecd69415b9f
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-05-01 02:29:24 +0530

    Time: 429 ms (42.90%), Space: 94.5 MB (33.58%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,29 +1,56 @@
 class Solution {
-
 public:
+    
+    long long help(long long mid , vector<int>&time)
+    {
+        long long trips = 0;
+        for(int i=0;i<time.size();i++)
+        {
+            trips += mid/time[i];
+        }
+        
+        return trips;
+        
+    }
+    
     long long minimumTime(vector<int>& time, int totalTrips) {
         
-         long long x= time[0],y = totalTrips;   
-        long long l=1,r= x*y,mid;
-
-        while(l<=r)
-        {
-             mid = (l+r)>>1;
-
-            long long sum=0;
-            for(auto x : time)
-                    sum+=(mid)/x;
-
-            if(sum>=totalTrips)
-             r = mid-1;
-
-            else l=mid+1;
-
-        }
-
-        return l;
+        if(time.size() == 1)
+            return totalTrips*1ll*time[0];
         
-
+        long long left = *min_element(time.begin(),time.end());
+        // long long right = *max_element(time.begin(),time.end());
+        long long right = left*totalTrips;
+        
+        while(left <= right)
+        {
+            long long mid = left+(right-left)/2;
+            
+            if(help(mid,time) < totalTrips)
+            {
+                left = mid+1;
+            }
+            else
+            {
+                if(mid == right || help(mid-1,time) < totalTrips)
+                {
+                    return mid;
+                }
+                
+                right = mid-1;
+            }
+        }
+        
+        return -1;
         
     }
 };
+
+// 1 2 3 4 5 6 7 8 9
+
+// 100000 .... 1000000000000
+
+
+    
+
+    
