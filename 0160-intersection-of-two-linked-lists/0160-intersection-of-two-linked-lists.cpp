849ad6973d4870712806f48a0517f2d47407e273
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2023-05-01 01:20:14 +0530

    Time: 51 ms (46.29%), Space: 14.6 MB (36.60%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -11,50 +11,93 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
-        vector<ListNode*>arr1,arr2;
-        ListNode* temp = headA;
+//         vector<ListNode*>arr1,arr2;
+//         ListNode* temp = headA;
         
+//         while(headA != NULL)
+//         {
+//             if(headA!= NULL)
+//             arr1.push_back(headA);
+//             headA = headA->next;
+//         }
+        
+//         while(headB != NULL)
+//         {
+//             if(headB != NULL)
+//             arr2.push_back(headB);
+//             headB = headB->next;
+//         }
+        
+//         // for(ListNode* x : arr1)
+//         //     cout<<x->val<<" ";
+//         // cout<<endl;
+//         // for(ListNode* x : arr2)
+//         //     cout<<x->val<<" ";
+//         // cout<<endl;
+        
+//         int i = arr1.size()-1;
+//         int j = arr2.size()-1;
+        
+//         while(i>=0 && j>=0)
+//         {
+//             if(arr1[i] != arr2[j])
+//             {
+//                 return arr1[i] -> next;
+//             }
+            
+//             i--;j--;
+//             if(i<0 && j<0)
+//                 return temp;
+//             if(j < 0)
+//                 return arr1[i]->next;
+//             if(i < 0)
+//                 return arr2[j]->next;
+                
+//         }
+        
+//         return temp;
+        if(!headA || !headB)
+            return NULL;
+        
+        ListNode*temp1 = headA;
+        ListNode*temp2 = headB;
+        int len1 = 0;
         while(headA != NULL)
         {
-            if(headA!= NULL)
-            arr1.push_back(headA);
+            len1++;
             headA = headA->next;
         }
-        
+        int len2 = 0;
         while(headB != NULL)
         {
-            if(headB != NULL)
-            arr2.push_back(headB);
+            len2++;
             headB = headB->next;
         }
         
-        // for(ListNode* x : arr1)
-        //     cout<<x->val<<" ";
-        // cout<<endl;
-        // for(ListNode* x : arr2)
-        //     cout<<x->val<<" ";
-        // cout<<endl;
-        
-        int i = arr1.size()-1;
-        int j = arr2.size()-1;
-        
-        while(i>=0 && j>=0)
+        if(len1 > len2)
         {
-            if(arr1[i] != arr2[j])
+            int diff = len1 - len2;
+            while(diff--)
             {
-                return arr1[i] -> next;
+                temp1 = temp1->next;
             }
-            
-            i--;j--;
-            if(i<0 && j<0)
-                return temp;
-            if(j < 0)
-                return arr1[i]->next;
-            if(i < 0)
-                return arr2[j]->next;
-                
+        }
+        else
+        {
+            int diff = len2 - len1;
+            while(diff--)
+            {
+                temp2 = temp2->next;
+            }
         }
         
-        return temp;
+        while(temp1 != temp2)
+        {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        
+        return temp1;
+        
     }
 };
