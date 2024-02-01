92a2fe72036ff3658140d806a83bfed533b763c0
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2024-02-01 21:22:33 +0530

    Time: 6 ms (80.13%), Space: 15.5 MB (40.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<long long> st ; 
-        for(string x : tokens)
+        
+        stack<int> st;
+        
+        for(int i=0;i<tokens.size();i++)
         {
-            if(x=="+"){
-                long long a = st.top() ;
-                st.pop() ;
-                long long b = st.top() ;
-                st.pop() ;
-                st.push(a+b) ;
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*")
+            {
+                int x = st.top();
+                st.pop();
+                int y = st.top();
+                st.pop();
+                
+                int z = 0;
+                if(tokens[i] == "+")
+                    z = x + y;
+                if(tokens[i] == "-")
+                    z = y-x;
+                if(tokens[i] == "*")
+                    z = x * y;
+                if(tokens[i] == "/")
+                    z = y/x;
+                
+                st.push(z);
             }
-            else if(x=="*"){
-                long long a = st.top() ;
-                st.pop() ;
-                long long b = st.top() ;
-                st.pop() ;
-                st.push(a*b) ;
+            else
+            {
+                int z = stoi(tokens[i]);
+                st.push(z);
             }
-            else if(x=="-"){
-                long long a = st.top() ;
-                st.pop() ;
-                long long b = st.top() ;
-                st.pop() ;
-                st.push(b-a) ;
-            }
-            else if(x=="/"){
-                long long a = st.top() ;
-                st.pop() ;
-                long long b = st.top() ;
-                st.pop() ;
-                st.push(b/a) ;
-            }
-            else{
-                st.push(stoi(x)) ;
-            }
+            
         }
-        return (int) st.top() ;
+        
+        return st.top();
     }
 };
