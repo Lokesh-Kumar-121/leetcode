3decdfa8f91c041a937d9c2859c40594958b3332
Lokesh-Kumar-121 <99606637+Lokesh-Kumar-121@users.noreply.github.com>
2024-02-07 23:22:54 +0530

    Time: 11 ms (98.60%), Space: 11.8 MB (59.95%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
-      unordered_map<char,int>m;
-        
-        for(int i=0;i<s.size();i++)
-        {
-            m[s[i]]=i;
+        int arr[26]={0};
+        for(int i=0;i<s.length();i++){
+            int ind = s[i]- 'a';
+            arr[ind]++;
         }
         
-        // for(auto x:m)
-        // {
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
-        
-        unordered_map<char,int>m2;
-        for(auto x:s)
-        {
-            m2[x]++;
+        for(int i=0;i<s.length();i++){
+            if( arr[s[i] - 'a'] ==1) return i;
         }
-        int mn=INT_MAX;
-        int ind;
-        int flag=0;
-        for(auto x:m2)
-        {
-            if(x.second==1)
-            {
-                flag=1;
-                auto it=m.find(x.first);
-                ind=it->second;
-                mn=min(mn,ind);
-                
-            }
-        }
-        if(flag==0)
-            return -1;
         
-        return mn;
+        return -1;
     }
 };
